80b5a174f39e18c2dae660b490dc21ce85b91c44
skyen <mkmoen@proton.me>
2025-12-01 10:02:43 +0000

    feat: dynamic metadata title

--- app/(pages)/[kontor]/layout.tsx
+++ app/(pages)/[kontor]/layout.tsx
@@ -1,9 +1,20 @@
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 type props = {
   children: React.ReactNode;
   params: Promise<{ kontor: string }>;
 };
+
+export async function generateMetadata({
+  params,
+}: Pick<props, "params">): Promise<Metadata> {
+  const { kontor } = await params;
+  const kontorTitle = kontor === "alta" ? "Alta" : "Bodø";
+  return {
+    title: `Din eiendomsmegler i ${kontorTitle}`,
+  };
+}
 
 export default async function Layout({ children, params }: props) {
   const { kontor } = await params;
